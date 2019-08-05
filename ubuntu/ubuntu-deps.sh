b09507a4098e1a838227de12dcc4a268897b1c23
Alex Kubacki <alex@aptible.com>
2019-08-05 10:29:33 -0400

    Always install ruby, bundler, psql client

--- ubuntu/ubuntu-deps.sh
+++ ubuntu/ubuntu-deps.sh
@@ -1,5 +1,9 @@
 apt-get update \
-    && apt-get install -y ruby python
+    && apt-get install -y ruby python python-pip \
+    && pip install virtualenv && pip install apt-select
+sudo apt-select
 add-apt-repository -y ppa:git-core/ppa \
     && apt-get update \
     && apt-get install -y git
+apt-get install -y ruby-bundler ruby-dev
+apt-get install -y postgresql-client-10
